9b0bde78432e626e882a4f8f28aea95861875347
Giovanni <gioromero5687@gmail.com>
2023-10-02 17:19:01 -0700

    react-todo week 1 assignment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState([{
+    id: 1,
+    title: 'complete assignment'
+  },
+  {
+    id: 2,
+    title: 'push assignment to Github'
+  }])
+
+  const [inputText, setInputText] = useState('')
+
+  useEffect(() => {
+    console.log(inputText)
+  }, [inputText])
+
+  useEffect(() => {
+    console.log(todoList)
+  }, [todoList])
+
+  const textChange = (e) => {
+    setInputText(e.target.value)
+  }
+
+  const submitItem = (e) => {
+
+    let newList = todoList;
+    newList.push({ id: todoList.length + 1, title: inputText })
+    setTodoList(newList)
+    setInputText('')
+    e.target.previousElementSibling.value = ''
+  }
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Todo List</h1>
+      <label htmlFor='search'>submit next to do item:</label>
+      <input id='todoItem' onChange={textChange} type='text' />
+      <button id='submit' onClick={submitItem}>Submit</button>
+
+      <div>
+        <ul>
+          {todoList.map((item) => {
+            console.log(item)
+            return <li id={item.id - 1}>{item.id} {item.title}</li>
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
